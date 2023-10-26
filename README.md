c9f76e4e80696a9be09868330feb03372d38cf7a
Alex <format37@gmail.com>
2023-10-26 14:28:46 +0400

    modified:   README.md 	modified:   server/server.py

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # panthera bot
 Conversational telegram bot docker server  
-![Structure](assets/structure_v1.png)
+![Structure](assets/structure_v1.png)  
+* [Telebot server](https://github.com/format37/telegram_bot)  
+* [Panthera bot](https://github.com/format37/pantherabot)  
+* [LLM service](https://github.com/format37/openai_proxy)
--- server/server.py
+++ server/server.py
@@ -24,44 +24,6 @@ async def call_test():
     return JSONResponse(content={"status": "ok"})
 
 
-"""def save_user_session(user_id, session):
-    logger.info(f'save_user_session: {user_id}')
-    # Save the user json file
-    path = './data/users'
-    user_path = os.path.join(path, f'{user_id}.json')
-    json.dump(session, open(user_path, 'w'))
-
-
-def get_user_session(user_id):
-    logger.info(f'get_user_session: {user_id}')
-    
-    # Check is the usef json file exist
-    path = './data/users'
-    user_path = os.path.join(path, f'{user_id}.json')    
-    if not os.path.exists(user_path):
-        default_path = os.path.join(path, 'default.json')
-        session = json.load(open(default_path, 'r'))
-        # Save the user json file
-        save_user_session(user_id, session)
-
-    session = json.load(open(user_path, 'r'))
-    # Return the user json file as dict
-    return session
-
-
-def log_message(message):
-    logger.info(f'message: {message}')
-    # Read the chat id from the message
-    chat_id = message['chat']['id']
-    # Prepare a folder
-    path = f'./data/chats/{chat_id}'
-    os.makedirs(path, exist_ok=True)
-    filename = f'{message["date"]}_{message["message_id"]}.json'
-    # Save the user json file
-    file_path = os.path.join(path, filename)
-    json.dump(message, open(file_path, 'w'))"""
-
-
 @app.post("/message")
 async def call_message(request: Request):
     logger.info('call_message')
