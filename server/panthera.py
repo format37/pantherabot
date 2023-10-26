0026ca1a1aca2732b3e5079aa3170bf3fac15ab2
Alex <format37@gmail.com>
2023-10-26 14:17:55 +0400

    modified:   server/Dockerfile 	new file:   server/panthera.py 	modified:   server/server.py

--- server/panthera.py
+++ server/panthera.py
@@ -1,16 +1,7 @@
-from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import JSONResponse
 import os
 import logging
 import json
-from panthera import (
-    save_user_session, 
-    get_user_session, 
-    log_message
-    )
 
-# Initialize FastAPI
-app = FastAPI()
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
@@ -18,13 +9,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-@app.get("/test")
-async def call_test():
-    logger.info('call_test')
-    return JSONResponse(content={"status": "ok"})
-
-
-"""def save_user_session(user_id, session):
+def save_user_session(user_id, session):
     logger.info(f'save_user_session: {user_id}')
     # Save the user json file
     path = './data/users'
@@ -59,36 +44,4 @@ def log_message(message):
     filename = f'{message["date"]}_{message["message_id"]}.json'
     # Save the user json file
     file_path = os.path.join(path, filename)
-    json.dump(message, open(file_path, 'w'))"""
-
-
-@app.post("/message")
-async def call_message(request: Request):
-    logger.info('call_message')
-    message = await request.json()
-    logger.info(message)
-    """
-    INFO:server:{
-       'message_id': 22,
-       'from': {
-            'id': 106129214, 
-            'is_bot': False, 
-            'first_name': 'Alex', 
-            'username': 'format37', 
-            'language_code': 'en', 
-            'is_premium': True
-         }, 
-        'chat': {
-            'id': 106129214, 
-            'first_name': 'Alex', 
-            'username': 'format37', 
-            'type': 'private'
-        }, 
-        'date': 1698311200, 
-        'text': '9'
-    }
-    """
-    user_session = get_user_session(message['from']['id'])
-    log_message(message)
-    logger.info(f'user_session: {user_session}')
-    return JSONResponse(content={"status": "ok"})
+    json.dump(message, open(file_path, 'w'))
