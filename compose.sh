3206e4a4b7d8863816a8130dda3bb9d33eed7deb
Alex <format37@gmail.com>
2024-06-04 10:26:43 +0400

    modified:   compose.sh

--- compose.sh
+++ compose.sh
@@ -1,5 +1,2 @@
-# sudo docker-compose up --build --force-recreate -d --remove-orphans
-sudo docker-compose down -v
-# sudo docker-compose up --build -d
-sudo docker-compose up --build --force-recreate -d --remove-orphans
-# sudo docker-compose up --build
+sudo docker compose down -v
+sudo docker compose up --build --force-recreate --remove-orphans -d
