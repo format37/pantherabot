e4548f561bfb31beaf300c2e7813723f4e32b11f
Alex <format37@gmail.com>
2024-06-06 16:09:27 +0400

    modified:   server/panthera.py

--- server/panthera.py
+++ server/panthera.py
@@ -188,7 +188,7 @@ class ChatAgent:
             name="image_context_conversation",
             description="Answering on your text request about provided images",
             args_schema=image_context_conversation_args,
-            return_direct=False,
+            return_direct=True,
         )
 
         tools = []
