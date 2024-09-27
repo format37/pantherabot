8d226b00efee585456c5af1b6ab74e9dd376dee7
Alex <format37@gmail.com>
2024-09-27 11:49:13 +0400

    modified:   server/panthera.py

--- server/panthera.py
+++ server/panthera.py
@@ -356,7 +356,7 @@ For the formatting you can use the telegram MarkdownV2 format. For example: {mar
         self.logger.info(f"[ask_reasoning] request")
         client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
         response = client.chat.completions.create(
-        model="o1-mini",
+        model="o1-preview",
             messages=[
                 {
                     "role": "user", 
