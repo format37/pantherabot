ecbfc29644f165ac6476ed08a106b2df1f75ba15
Alex <format37@gmail.com>
2024-10-20 12:51:20 +0400

    modified:   server/panthera.py

--- server/panthera.py
+++ server/panthera.py
@@ -105,15 +105,15 @@ class ChatAgent:
         # model = 'gpt-4o-2024-05-13'
         model = 'gpt-4o'
         temperature = 0.7
-        llm = ChatOpenAI(
-            openai_api_key=os.environ.get('OPENAI_API_KEY', ''),
-            model=model,
-            temperature=temperature,
-        )
-
-        # llm = ChatAnthropic(
-        #     model="claude-3-5-sonnet-20240620"  # Specify the model name you want to use
+        # llm = ChatOpenAI(
+        #     openai_api_key=os.environ.get('OPENAI_API_KEY', ''),
+        #     model=model,
+        #     temperature=temperature,
         # )
+
+        llm = ChatAnthropic(
+            model="claude-3-5-sonnet-20240620"  # Specify the model name you want to use
+        )
         # if "ANTHROPIC_API_KEY" not in os.environ:
         #     self.logger.error("ANTHROPIC_API_KEY is not set")
 
