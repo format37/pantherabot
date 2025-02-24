d30b9a88e11a495344eb961bd834cf3955147f39
Alex <format37@gmail.com>
2025-02-24 14:01:52 +0400

    Model updated to Claude 	modified:   server/panthera.py

--- server/panthera.py
+++ server/panthera.py
@@ -109,23 +109,23 @@ class ChatAgent:
         # model = 'gpt-4o-2024-11-20'
         # model = 'o1-preview'
         # model = 'o1-mini'
-        temperature = 1.0
-        llm = ChatOpenAI(
-            openai_api_key=os.environ.get('OPENAI_API_KEY', ''),
-            model=model,
-            # temperature=temperature,
-        )
-
-        # if "ANTHROPIC_API_KEY" not in os.environ:
-        #     self.logger.error("ANTHROPIC_API_KEY is not set")
-        # # model="claude-3-5-sonnet-20240620",
         # temperature = 1.0
-        # model="claude-3-5-sonnet-20241022",  # Specify the model name you want to use
-        # llm = ChatAnthropic(
-        #     model="claude-3-5-sonnet-20241022",
-        #     temperature = temperature,
-        #     max_tokens=8192,
-        # )        
+        # llm = ChatOpenAI(
+        #     openai_api_key=os.environ.get('OPENAI_API_KEY', ''),
+        #     model=model,
+        #     # temperature=temperature,
+        # )
+
+        if "ANTHROPIC_API_KEY" not in os.environ:
+            self.logger.error("ANTHROPIC_API_KEY is not set")
+        # model="claude-3-5-sonnet-20240620",
+        # temperature = 1.0
+        model="claude-3-5-sonnet-20241022",  # Specify the model name you want to use
+        llm = ChatAnthropic(
+            model="claude-3-5-sonnet-20241022",
+            # temperature = temperature,
+            max_tokens=8192,
+        )        
 
         # temperature=0.7,
         #     max_tokens=150,
