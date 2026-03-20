4edc99906147fbd8afe1d82760139600800a2d07
Alex <format37@gmail.com>
2026-03-20 14:08:42 +0400

    logs command fix

--- logs.sh
+++ logs.sh
@@ -1,7 +1,1 @@
-# get id of running container with image name
-# container_id=$(sudo docker ps | grep pantherabot_server_1 | awk '{print $1}')
-# echo "container id: $container_id"
-# get logs
-sudo docker logs -f panthera
-# connect to container
-# sudo docker exec -it $container_id /bin/bash
+docker compose logs -f -t
